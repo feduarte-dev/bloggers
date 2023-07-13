103043c6220927d4971806ada94bbc952b22825f
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-07-13 10:38:11 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles/App.css';
+import { About, NotFound, Posts, Users } from './pages';
+
+function App() {
+  return (<p>WIP</p>);
+}
+
+export default App;
